3aefb19ea97e6e212091efdd7811b5bf69475b72
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-06-14 15:52:11 -0400

    Add stylesheets

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -97,8 +97,144 @@ export default function Info() {
     setResponsibilities(e.target.value);
   }
   return (
-    <>
-      <p>hinpm </p>
-    </>
+    <div className="info">
+      <header className="header">
+        <h1>CV Creator Project</h1>
+        <p>Enter your info and we'll take care of the rest!</p>
+      </header>
+
+      {/* THIS IS THE OLD INPUT FIELDS LIST */}
+      {/*
+      <div className="buttons-and-input-container">
+        <InputFieldsList
+          inputFields={inputFields}
+          selectedId={state.selectedId}
+          dispatch={dispatch}
+          className="input-fields-list"
+        /> 
+
+        <Text
+          key={inputFields.id}
+          message={message}
+          inputField={inputFields}
+          dispatch={dispatch}
+          onChange={handleTheChange}
+          value={state.userInput}
+          type="text"
+        />
+        <p>{state.value}</p>
+      </div>
+      */}
+
+      <div class="user-input">
+        <div class="general-info">
+          <h3>General Info</h3>
+          {/* expand or collapse the "General Info" section on button click using a useState */}
+          <button onClick={handleEditGeneralInfoClick}>
+            {" "}
+            {editGeneralInfo ? "Submit" : "Edit"}{" "}
+          </button>
+          {editGeneralInfo && (
+            <>
+              <label>
+                First name:{" "}
+                <input value={firstName} onChange={handleFirstNameChange} />
+              </label>
+              <label>
+                Last name:{" "}
+                <input value={lastName} onChange={handleLastNameChange} />
+              </label>
+              <label>
+                Email: <input value={email} onChange={handleEmailChange} />
+              </label>
+              <label>
+                Phone number:{" "}
+                <input value={phoneNumber} onChange={handlePhoneNumberChange} />
+              </label>
+            </>
+          )}
+        </div>
+
+        <div class="education-exp">
+          <h3>Educational Experience</h3>
+          <button onClick={handleEditEducationClick}>
+            {" "}
+            {editEducation ? "Submit" : "Edit"}{" "}
+          </button>
+          {editEducation && (
+            <>
+              <label>
+                School name:{" "}
+                <input value={schoolName} onChange={handleSchoolNameChange} />
+              </label>
+              <label>
+                Field of study:{" "}
+                <input
+                  value={fieldOfStudy}
+                  onChange={handleFieldOfStudyChange}
+                />
+              </label>
+              <label>
+                Years of study:{" "}
+                <input
+                  value={yearsOfStudy}
+                  onChange={handleYearsOfStudyChange}
+                />
+              </label>
+            </>
+          )}
+        </div>
+
+        <div class="practical-exp">
+          <h3>Practical Experience</h3>
+          <button onClick={handleEditExperienceClick}>
+            {" "}
+            {editExperience ? "Submit" : "Edit"}{" "}
+          </button>
+          {editExperience && (
+            <>
+              <label>
+                Company name:{" "}
+                <input value={companyName} onChange={handleCompanyNameChange} />
+              </label>
+              <label>
+                Position title:{" "}
+                <input
+                  value={positionTitle}
+                  onChange={handlePositionTitleChange}
+                />
+              </label>
+              <label>
+                Responsibilities:{" "}
+                <textarea
+                  value={responsibilities}
+                  onChange={handleResponsibilitiesChange}
+                />
+              </label>
+            </>
+          )}
+        </div>
+      </div>
+
+      {/* THE CV RETURNED USING THE FORM DATA */}
+      <div id="cv-form">
+        <h2>{fullName}</h2>
+        <span>
+          {phoneNumber} {email}{" "}
+        </span>
+
+        <h3>Educational Experience</h3>
+        <p>
+          <b>{schoolName}</b>
+        </p>
+        <p>{fieldOfStudy}</p>
+        <p>{yearsOfStudy}</p>
+        <h3>Practical Experience</h3>
+        <p>{companyName}</p>
+        <p>{positionTitle}</p>
+        <p>Responsibilities:</p>
+        <li>{responsibilities}</li>
+      </div>
+    </div>
   );
 }
