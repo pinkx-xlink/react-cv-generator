296a6f64c2d0d0633fb35c370607f35a5c4be850
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-06-18 13:39:27 -0400

    Reposition the footer, and rename it to .footer-container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Info from "./components/Info";
 import Header from "./components/Header";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Info />
+      <Footer />
     </div>
   );
 }
